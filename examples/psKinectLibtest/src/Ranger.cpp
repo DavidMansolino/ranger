769af4ca4628ab940aedf78528756f79cd90b0ed
Inaki Navarro <inaki.navarro@epfl.ch>
2014-07-14 18:47:01 +0200

    library kinect fixed

--- examples/psKinectLibtest/src/Ranger.cpp
+++ examples/psKinectLibtest/src/Ranger.cpp
@@ -20,11 +20,8 @@ static WbDeviceTag kinect;
 //Constructor
 Ranger::Ranger()
 {
-
   kinect = wb_kinect_get("kinect");
   wb_kinect_enable(kinect, TIME_STEP, 2*TIME_STEP, false);
-
-
 }
 
 //Destructor
@@ -34,11 +31,9 @@ Ranger::~Ranger() {
 
 void Ranger::run() {
   while(1) {
-
-  const short unsigned int *rangeImage_mm = wb_kinect_get_range_image_mm(kinect);
-  int centerpix = (wb_kinect_get_range_height(kinect)+1)*wb_kinect_get_range_width(kinect)/2;
-  int distance = rangeImage_mm[centerpix];
-  printf("Distance: %d\n", distance);
-
+    const short unsigned int *rangeImage_mm = wb_kinect_get_range_image_mm(kinect);
+    int centerpix = (wb_kinect_get_range_height(kinect)+1)*wb_kinect_get_range_width(kinect)/2;
+    int distance = rangeImage_mm[centerpix];
+    printf("Distance: %d\n", distance);
   }
 }
