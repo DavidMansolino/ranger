072e61521085e42d715f6940025f5b5f061a61c3
Inaki Navarro <inaki.navarro@epfl.ch>
2014-10-13 11:35:43 +0200

    added psImaread

---
+++ examples/psImageRead/main.cpp
@@ -0,0 +1,133 @@
+/*****************************************************************************
+*                                                                            *
+*  OpenNI 2.x Alpha                                                          *
+*  Copyright (C) 2012 PrimeSense Ltd.                                        *
+*                                                                            *
+*  This file is part of OpenNI.                                              *
+*                                                                            *
+*  Licensed under the Apache License, Version 2.0 (the "License");           *
+*  you may not use this file except in compliance with the License.          *
+*  You may obtain a copy of the License at                                   *
+*                                                                            *
+*      http://www.apache.org/licenses/LICENSE-2.0                            *
+*                                                                            *
+*  Unless required by applicable law or agreed to in writing, software       *
+*  distributed under the License is distributed on an "AS IS" BASIS,         *
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  *
+*  See the License for the specific language governing permissions and       *
+*  limitations under the License.                                            *
+*                                                                            *
+*****************************************************************************/
+#include <stdio.h>
+#include <OpenNI.h>
+
+#include "OniSampleUtilities.h"
+
+
+
+#define SAMPLE_READ_WAIT_TIMEOUT 2000 //2000ms
+
+using namespace openni;
+
+int main(int argc, char *argv[])
+{
+
+	int filter_value = 0; 
+
+
+	if (argc >1){
+		filter_value = atoi(argv[1]);
+	}
+
+	printf("Filter: %d\n",filter_value);
+
+	Status rc = OpenNI::initialize();
+	if (rc != STATUS_OK)
+	{
+		printf("Initialize failed\n%s\n", OpenNI::getExtendedError());
+		return 1;
+	}
+
+	Device device;
+	rc = device.open(ANY_DEVICE);
+	if (rc != STATUS_OK)
+	{
+		printf("Couldn't open device\n%s\n", OpenNI::getExtendedError());
+		return 2;
+	}
+
+	VideoStream depth;
+
+	if (device.getSensorInfo(SENSOR_DEPTH) != NULL)
+	{
+		rc = depth.create(device, SENSOR_DEPTH);
+		if (rc != STATUS_OK)
+		{
+			printf("Couldn't create depth stream\n%s\n", OpenNI::getExtendedError());
+			return 3;
+		}
+	}
+
+	rc = depth.start();
+	if (rc != STATUS_OK)
+	{
+		printf("Couldn't start the depth stream\n%s\n", OpenNI::getExtendedError());
+		return 4;
+	}
+
+	VideoFrameRef frame;
+
+	while (!wasKeyboardHit())
+	{
+		int changedStreamDummy;
+		int i,j;
+		int shift;
+		VideoStream* pStream = &depth;
+		rc = OpenNI::waitForAnyStream(&pStream, 1, &changedStreamDummy, SAMPLE_READ_WAIT_TIMEOUT);
+		if (rc != STATUS_OK)
+		{
+			printf("Wait failed! (timeout is %d ms)\n%s\n", SAMPLE_READ_WAIT_TIMEOUT, OpenNI::getExtendedError());
+			continue;
+		}
+
+		rc = depth.readFrame(&frame);
+		if (rc != STATUS_OK)
+		{
+			printf("Read failed!\n%s\n", OpenNI::getExtendedError());
+			continue;
+		}
+
+		if (frame.getVideoMode().getPixelFormat() != PIXEL_FORMAT_DEPTH_1_MM && frame.getVideoMode().getPixelFormat() != PIXEL_FORMAT_DEPTH_100_UM)
+		{
+			printf("Unexpected frame format\n");
+			continue;
+		}
+
+		DepthPixel* pDepth = (DepthPixel*)frame.getData();
+
+		for (j=0;j<frame.getHeight();j++){
+			printf("[%3d]", j);
+	  		shift = frame.getWidth()*j; //frame.getWidth()/2;
+			for(i=0;i<frame.getWidth();i+=1){
+				if (filter_value){
+					if (pDepth[shift+i] > 0){
+						printf("1");
+					} else {
+						printf("0");
+					}
+				} else {
+					printf(" %5d", pDepth[shift+i]);
+				}
+			}
+			printf("\n");
+		}
+		break;
+	}
+
+	depth.stop();
+	depth.destroy();
+	device.close();
+	OpenNI::shutdown();
+
+	return 0;
+}
